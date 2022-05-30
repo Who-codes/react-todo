cb23a9a64a0b808f5e142d3f265048dc928e00ef
Sulav <sulavshrestha023@gmail.com>
2022-05-30 18:17:16 +0545

    form text state set with style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>this is app</h1>;
+  const [list, setList] = useState([]);
+  const [text, setText] = useState("");
+  const [alert, setAlert] = useState(false);
+  const [Edit, setEdit] = useState({ status: false, msg: "", type: "" });
+  const [editId, setEditId] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
+  return (
+    <div className="container">
+      <h2>to do list</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="text"
+          placeholder="Eg. Submit Assignment By 11am"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button className="add-btn">Add</button>
+      </form>
+      <section className="center"></section>
+    </div>
+  );
 }
 
 export default App;
